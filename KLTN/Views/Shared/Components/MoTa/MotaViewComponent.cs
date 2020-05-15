ec8a6f55674ef17671f8da86cae3c79d4a1eab86
TaVanHa <tavanha090@gmail.com>
2020-05-15 14:45:06 +0700

    modified table 2

---
+++ KLTN/Views/Shared/Components/MoTa/MotaViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace KLTN.Views.Shared.Components.MoTa
+{
+    //[ViewComponent(Name = "MoTa")]
+    public class MotaViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
