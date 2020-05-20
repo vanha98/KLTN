d1b7762f47667f10758c0a20895e5c301b105abe
dinhtuanhai <dinhtuanhai1298@gmail.com>
2020-05-20 16:57:29 +0700

    Update layout ThaoLuan(GVHD)

---
+++ KLTN/Views/Shared/Components/BtnTaoBaiDang/BtnTaoBaiDangViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLTN.Views.Shared.Components.BtnTaoBaiDang
+{
+    public class BtnTaoBaiDangViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
