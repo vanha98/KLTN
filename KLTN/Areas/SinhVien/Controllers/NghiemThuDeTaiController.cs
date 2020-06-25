b5eee2fd8c576bd0c7ce348d505d8df6b619cfa5
dinhtuanhai <dinhtuanhai1298@gmail.com>
2020-06-25 09:59:14 +0700

    Create XetDuyet, NghiemThu view

---
+++ KLTN/Areas/SinhVien/Controllers/NghiemThuDeTaiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KLTN.Areas.SinhVien.Controllers
+{
+    [Area("SinhVien")]
+    public class NghiemThuDeTaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
