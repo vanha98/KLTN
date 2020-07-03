171b6145df421dab6e3a393bf75095f66d6ddf18
dinhtuanhai <dinhtuanhai1298@gmail.com>
2020-07-03 12:42:51 +0700

    detainghiencuu

--- KLTN/Areas/Admin/Controllers/DeTaiNghienCuuController.cs
+++ KLTN/Areas/Admin/Controllers/DeTaiNghienCuuController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Data.Enum;
 using Data.Interfaces;
 using Data.Models;
+using KLTN.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KLTN.Areas.Admin.Controllers
@@ -22,9 +23,22 @@ namespace KLTN.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             IEnumerable<MoDot> listDotDangKy = await _serviceMoDot.GetAll(x => x.Loai == (int)MoDotLoai.DangKy);
-            IEnumerable<DeTaiNghienCuu> listDeTai = await _service.GetAll(x => x.TinhTrangDeTai == (int)StatusDeTai.DaDuyet && x.TinhTrangDeTai == (int)StatusDeTai.DaDangKy);
-            
-            return View();
+            MoDot DotDangKyMoiNhat =  listDotDangKy.ToList().Last();
+
+            IEnumerable<DeTaiNghienCuu> listDeTaiHienTai = await _service.GetAll(x => x.TinhTrangDeTai == (int)StatusDeTai.DaDuyet 
+                                                                                 && x.TinhTrangDeTai == (int)StatusDeTai.DaDangKy
+                                                                                 && (x.NgayDangKy > DotDangKyMoiNhat.ThoiGianBd
+                                                                                 && x.NgayDangKy < DotDangKyMoiNhat.ThoiGianKt)
+                                                                                 || (x.NgayDangKy == null && x.TinhTrangDeTai == (int)StatusDeTai.DaDuyet));
+            IEnumerable<DeTaiNghienCuu> listDeTaiDeXuatHienTai = await _service.GetAll(x => x.Loai == LoaiDeTai.DeXuat
+                                                                                       && x.NgayDangKy > DotDangKyMoiNhat.ThoiGianBd
+                                                                                       && x.NgayDangKy < DotDangKyMoiNhat.ThoiGianKt);
+
+            DeTaiNghienCuuAdminViewModel viewModel = new DeTaiNghienCuuAdminViewModel() {
+                listDeTaiHienTai = listDeTaiHienTai,
+                listDeTaiDeXuatHienTai = listDeTaiDeXuatHienTai
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ KLTN/Areas/Admin/Models/DeTaiNghienCuuAdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLTN.Areas.Admin.Models
+{
+    public class DeTaiNghienCuuAdminViewModel
+    {
+        public IEnumerable<DeTaiNghienCuu> listDeTaiHienTai { get; set; }
+        public IEnumerable<DeTaiNghienCuu> listDeTaiDeXuatHienTai { get; set; }
+    }
+}
